b0a3b19448c4b3173e04e1e276b6c58c2ed2f10c
Rene Seckler <rene.seckler@esbyte.de>
2021-02-17 13:08:38 +0100

    óops

--- src/index.js
+++ src/index.js
@@ -24,6 +24,13 @@ const voiceRole = require('./voiceRole')
 const ticketManager = require('./modules/tickets/ticketManager')
 const help = require('./commands/help')
 
+const activities_list = [
+    "For Rule Breakers",
+    "The purple names",
+    "#general",
+    "The mods do their job"
+]; // creates an arraylist containing phrases you want your bot to switch through.
+
 sequelize
   .authenticate()
   .then(() => {
@@ -50,19 +57,12 @@ client.on('ready', () => {
   commandManager.addModule('ticket', ticketManager)
 
 
-    const activities_list = [
-        "For Rule Breakers",
-        "The purple names",
-        "#general",
-        "The mods do their job"
-    ]; // creates an arraylist containing phrases you want your bot to switch through.
 
-    client.on('ready', () => {
+
         setInterval(() => {
             const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
             client.user.setActivity(activities_list[index], { type: 'WATCHING' }); // sets bot's activities to one of the phrases in the arraylist.
         }, 10000); // Runs this every 10 seconds.
-    });
 
 
   afkReply(client)
