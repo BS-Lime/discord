3749b65c68e9fd3da02745680c7b6e3ada72b792
Rene Seckler <rene.seckler@esbyte.de>
2021-02-17 07:09:16 +0100

    added admin check for talkback

--- src/commands/talkBack.js
+++ src/commands/talkBack.js
@@ -1,6 +1,11 @@
 const getPrefix = require('../util/getPrefix')
+const checkPerm = require('../util/checkPerm')
 
 module.exports = function (message, onlyPayload) {
+    if (!checkPerm(message, 'ADMINISTRATOR') || message.author.bot) {
+        return
+    }
+
   if (onlyPayload === '') {
     getPrefix(message).then(prefix => message.channel.send(`\`${prefix}talkBack <message>\``))
   } else {
