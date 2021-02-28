7d2c1937b7118504c86a0a0708b3d7bcf210573e
Rene Seckler <rene.seckler@esbyte.de>
2021-02-28 22:18:41 +0100

    grrrrrrrrrrrr

--- src/commands/alber.js
+++ src/commands/alber.js
@@ -7,5 +7,5 @@ const alber_quotes = [
 
 module.exports = function (message, onlyPayload) {
     const alber_index = Math.floor(Math.random() * (alber_quotes.length - 1) + 1);
-    message.channel.send(coyote_noises[alber_index] + ' - Alber');
+    message.channel.send(alber_quotes[alber_index] + ' - Alber');
 } 
